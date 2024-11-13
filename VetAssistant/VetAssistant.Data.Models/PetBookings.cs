2bf92475ea5d8125b07e9dcf5ec046fd0216a22f
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-11-13 22:13:25 +0200

    - Add mapping table PetBookings

--- VetAssistant/VetAssistant.Data.Models/PetBookings.cs
+++ VetAssistant/VetAssistant.Data.Models/PetBookings.cs
@@ -1,6 +1,25 @@
-﻿namespace VetAssistant.Data.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace VetAssistant.Data.Models
 {
     public class PetBookings
     {
+        [Required]
+        [Comment("PetId")]
+        public Guid PetId { get; set; }
+
+        [Required]
+        [Comment("Pet")]
+        public virtual Pet Pet { get; set; } = null!;
+
+        [Required]
+        [Comment("Booking")]
+        public Guid BookingId { get; set; }
+
+        [Required]
+        [Comment("Booking")]
+        public virtual Booking Booking { get; set; } = null!;
+
     }
 }
