1530ce3e9406c8267478189b791a9cd5d23d3e21
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-11-13 22:10:03 +0200

    - Add maping table DoctorPet

--- VetAssistant/VetAssistant.Data.Models/DoctorPet.cs
+++ VetAssistant/VetAssistant.Data.Models/DoctorPet.cs
@@ -1,6 +1,24 @@
-﻿namespace VetAssistant.Data.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace VetAssistant.Data.Models
 {
     public class DoctorPet
     {
+        [Required]
+        [Comment("DoctorId")]
+        public Guid DoctorId { get; set; }
+
+        [Required]
+        [Comment("Doctor")]
+        public virtual Doctor Doctor { get; set; } = null!;
+
+        [Required]
+        [Comment("PetId")]
+        public Guid PetId { get; set; }
+
+        [Required]
+        [Comment("Pet")]
+        public virtual Pet Pet { get; set; } = null!;
     }
 }
