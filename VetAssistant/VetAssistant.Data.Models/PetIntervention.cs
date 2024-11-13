7097158d1503980eb1a2df5b3aa308d1a7c2b144
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-11-13 22:16:15 +0200

    - Add PetIntervention mapping table

--- VetAssistant/VetAssistant.Data.Models/PetIntervention.cs
+++ VetAssistant/VetAssistant.Data.Models/PetIntervention.cs
@@ -1,6 +1,24 @@
-﻿namespace VetAssistant.Data.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace VetAssistant.Data.Models
 {
     public class PetIntervention
     {
+        [Required]
+        [Comment("PetId")]
+        public Guid PetId { get; set; }
+
+        [Required]
+        [Comment("Pet")]
+        public virtual Pet Pet { get; set; } = null!;
+
+        [Required]
+        [Comment("InterventionId")]
+        public Guid InterventionId { get; set; }
+
+        [Required]
+        [Comment("Intervention")]
+        public virtual Intervention Intervention { get; set; } = null!;
     }
 }
