8e4942aa2cda12cf41db5885516a2c75c98f3266
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-11-14 06:11:27 +0200

    - Add UserDetails Entity Model

--- VetAssistant/VetAssistant.Data.Models/UserDetails.cs
+++ VetAssistant/VetAssistant.Data.Models/UserDetails.cs
@@ -1,6 +1,56 @@
-﻿namespace VetAssistant.Data.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VetAssistant.Data.Models
 {
     public class UserDetails
     {
+        [Key]
+        [Comment("UserId")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Comment("FirstName")]
+        [MaxLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("LastName")]
+        [MaxLength(50)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("PhoneNumber")]
+        [MaxLength(50)]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("CountryId")]
+        public Guid CountryId { get; set; }
+
+        [Required]
+        [Comment("Country")]
+        [ForeignKey(nameof(CountryId))]
+        public Country Country { get; set; } = null!;
+
+
+        [Required]
+        [Comment("Town")]
+        [MaxLength(50)]
+        public string Town { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("Address")]
+        [MaxLength(50)]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        [Comment("ImageUrl")]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [Required]
+        [Comment("UserDetailsSoftDeleted")]
+        public bool IsDeleted { get; set; } = false;
     }
 }
