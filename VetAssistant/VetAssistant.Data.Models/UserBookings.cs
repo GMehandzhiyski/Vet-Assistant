ef77819a7f470e6ed94d3328dfe9f9d37c1b2911
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-11-14 05:58:13 +0200

    - Add Booking EntityTable

--- VetAssistant/VetAssistant.Data.Models/UserBookings.cs
+++ VetAssistant/VetAssistant.Data.Models/UserBookings.cs
@@ -1,6 +1,26 @@
-﻿namespace VetAssistant.Data.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace VetAssistant.Data.Models
 {
     public class UserBookings
     {
+        [Required]
+        [Comment("UserId")]
+        public Guid UserId { get; set; }
+
+        [Required]
+        [Comment("UserDetails")]
+        public virtual UserDetails UserDetails { get; set; } = null!;
+
+        [Required]
+        [Comment("BookingId")]
+        public Guid BookingId { get; set; }
+
+        [Required]
+        [Comment("Booking")]
+        public virtual Booking Booking { get; set; } = null!;
+
+
     }
 }
