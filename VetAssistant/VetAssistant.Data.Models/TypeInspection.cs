67638e7b38a29757bbcd60d52baf7ae88c92c795
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-11-14 05:53:56 +0200

    - Add TypeINspection Entity Table

--- VetAssistant/VetAssistant.Data.Models/TypeInspection.cs
+++ VetAssistant/VetAssistant.Data.Models/TypeInspection.cs
@@ -1,6 +1,19 @@
-﻿namespace VetAssistant.Data.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace VetAssistant.Data.Models
 {
     public class TypeInspection
     {
+        [Key]
+        [Comment("TypeInspectionId")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Comment("TypeInterventionName")]
+        public string Name { get; set; } = null!;
+
+        [Comment("TYpeInspectionCollection")]
+        public virtual ICollection<Type> Types { get; set; } = null!;
     }
 }
