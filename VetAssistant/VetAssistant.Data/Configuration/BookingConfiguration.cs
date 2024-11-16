8f676fe20f8a60ce0a2444b5868953be6f6e8dcc
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-11-16 10:15:58 +0200

    - Add configuration on Fluent Api for Entity Boking

---
+++ VetAssistant/VetAssistant.Data/Configuration/BookingConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VetAssistant.Data.Models;
+
+namespace VetAssistant.Data.Configuration
+{
+    public class BookingConfiguration : IEntityTypeConfiguration<Booking>
+    {
+        public void Configure(EntityTypeBuilder<Booking> builder)
+        {
+            builder
+                 .HasOne(b => b.UserDetails)
+                 .WithMany()
+                 .HasForeignKey(b => b.UserDetails)
+                 .OnDelete(DeleteBehavior.NoAction);
+
+            builder
+                .HasOne(b => b.Pet)
+                .WithMany()
+                .HasForeignKey(b => b.PetId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder
+                .HasOne(b => b.Doctor)
+                .WithMany()
+                .HasForeignKey(b => b.DoctorId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder
+                .HasOne(b => b.TypeInspection)
+                .WithMany()
+                .HasForeignKey(b => b.TypeInspectionId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+        }
+    }
+}
