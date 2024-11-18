b7ff7e5950be6017e1a7276c2b22f2a251453d18
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-11-18 07:46:58 +0200

    - Add Intervention Interface and Service

---
+++ VetAssistant/VetAssistant.Service.Data/HomeService.cs
@@ -0,0 +1,9 @@
+﻿using VetAssistant.Service.Data.Contracts;
+
+namespace VetAssistant.Service.Data
+{
+    public class HomeService : BaseService, IHomeService
+    {
+
+    }
+}
---
+++ VetAssistant/VetAssistant.Service.Data/InterventionService.cs
@@ -0,0 +1,16 @@
+﻿using VetAssistant.Data.Models;
+using VetAssistant.Data.Repository.Interfaces;
+using VetAssistant.Service.Data.Contracts;
+
+namespace VetAssistant.Service.Data
+{
+    public class InterventionService : BaseService, IInterventionService
+    {
+        private readonly IRepository<Intervention, Guid> interventionRepository;
+
+        public InterventionService(IRepository<Intervention, Guid> _interventionRepository)
+        {
+            interventionRepository = _interventionRepository;
+        }
+    }
+}
