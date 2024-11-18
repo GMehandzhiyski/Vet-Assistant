d628a253a314e3de201934954aa2caabbf3fe10c
Georgi Mehandzhiyski <114901045+GMehandzhiyski@users.noreply.github.com>
2024-11-18 07:37:09 +0200

    - Add Doctor Interface and Service

---
+++ VetAssistant/VetAssistant.Service.Data/DoctorService.cs
@@ -0,0 +1,16 @@
+﻿using VetAssistant.Data.Models;
+using VetAssistant.Data.Repository.Interfaces;
+using VetAssistant.Service.Data.Contracts;
+
+namespace VetAssistant.Service.Data
+{
+    public class DoctorService : BaseService, IDoctorService
+    {
+        private readonly IRepository<Doctor, Guid> doctorRepository;
+
+        public DoctorService(IRepository<Doctor, Guid> _doctorRepository)
+        {
+            doctorRepository = _doctorRepository;
+        }
+    }
+}
